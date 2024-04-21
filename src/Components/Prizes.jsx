08df9559887d06636465a1295e246082e57f65e9
harshraj2717 <harshraj2717@gmail.com>
2024-04-22 03:22:39 +0530

    update text in prizes section

--- src/Components/Prizes.jsx
+++ src/Components/Prizes.jsx
@@ -1,12 +1,11 @@
-function PrizeCard({ prize_heading, cash_amount, voucher_amount }) {
+function PrizeCard({ prize_heading, cash_amount, goodies_amount }) {
     return (
       <div className="border-8 z-50 bg-purple-400 shadow-gray-500 rounded-lg shadow-2xl border-purple-600 m-2 max-w-sm lg:w-96 p-4  transition-transform duration-300 ease-in hover:scale-105">
         <h2 className="text-3xl font-bold">{prize_heading}</h2>
         <p className="text-yellow-btn-primary text-2xl font-semibold">
           Cash Prize: ₹{cash_amount}
         </p>
-        <p className="text-xl font-semibold">Vouchers: ₹{voucher_amount}</p>
-        <p className="text-lg italic">+ Exciting Goodies</p>
+        <p className="text-lg italic">Goodies worth ₹{goodies_amount}+</p>
       </div>
     );
   }
@@ -26,34 +25,44 @@ function PrizeCard({ prize_heading, cash_amount, voucher_amount }) {
         <PrizeCard
             prize_heading="Winning Team"
             cash_amount="15000"
-            voucher_amount="20000"
+            goodies_amount="20000"
           />
         </div>
           <div  className="flex flex-col md:flex-row">
           <PrizeCard
             prize_heading="First Runner Up"
             cash_amount="8000"
-            voucher_amount="20000"
+            goodies_amount="20000"
           />
           <PrizeCard
             prize_heading="Second Runner Up"
             cash_amount="5000"
-            voucher_amount="20000"
+            goodies_amount="20000"
           />
           </div>
           <div  className="flex flex-col md:flex-row">
           <PrizeCard
             prize_heading="All Girls Team"
             cash_amount="1000"
-            voucher_amount="20000"
+            goodies_amount="20000"
           />
           <PrizeCard
             prize_heading="All Freshers Team"
             cash_amount="1000"
-            voucher_amount="20000"
+            goodies_amount="20000"
           />
           </div>
-          
+          <div className="flex flex-col md:flex-row md:justify-center md:items-center">
+            <div className="border-4 z-50 bg-purple-400 shadow-gray-500 rounded-lg shadow-2xl border-purple-600 m-2 p-2 transition-transform duration-300 ease-in hover:scale-105">
+              <p className="text-md italic">
+                + Exciting{" "}
+                <span className="font-bold text-lg" style={{ fontStyle: "normal" }}>
+                  Goodies and Giveaways
+                </span>{" "}
+                for all participants
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     );
