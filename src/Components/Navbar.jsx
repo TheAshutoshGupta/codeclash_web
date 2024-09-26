3a58dc051377beb4070ed5953c59649844849195
harshraj2717 <harshraj2717@gmail.com>
2024-09-27 00:12:17 +0530

    add shrinik logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import NavMenutoggle from './NavMenutoggle';
 import logoCircle from '../assets/logo_circle.png';
-import glb from '../assets/glbitm_logo.png'
+import glb from '../assets/glbitm_logo.png';
+import shrinik from '../assets/shrinik_logo.png';
 import ButtonRotatingBackgroundGradient from './ButtonRotatingBackgroundGradient';
 
 
@@ -10,6 +11,7 @@ const Navbar=({menuOpen, setMenuOpen})=> {
     <div className='grid grid-cols-3 justify-between items-center p-8 sticky z-[100]'>
       <div className='w-[200px] text-white text-2xl flex gap-2 font-bold'>
         <img src={logoCircle} alt='logo' className='h-12'/>
+        <img src={shrinik} alt='logo' className='h-12'/>
         <img src={glb} alt='logo' className='h-12 hidden md:block'/>
       </div>
 
